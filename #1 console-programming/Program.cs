11ead0471e3476f18ba1f168c0657f083ce3654d
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-01-19 14:13:11 +0300

    Console Programming lesson at Kodluyoruz

---
+++ #1 console-programming/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Adınızı Giriniz: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyadınızı Giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name +" " + surname);
+
+            Console.ReadKey();
+        }
+    }
+}
