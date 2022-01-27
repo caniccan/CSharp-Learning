e215a64045551037bd0bc9aa9680e194f0e74495
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-01-27 16:55:49 +0300

    Error Management lesson at Kodluyoruz

---
+++ #5 Error-Management/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _5_Error_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //     Console.WriteLine("Bir sayı giriniz");
+            //     int sayi = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Girmiş Olduğunuz Sayı :"+ sayi); 
+            // }
+            // catch(Exception ex)
+            // {
+            //     Console.WriteLine("Hata: "+ ex.Message.ToString());
+                
+                
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("İşlem Tamamlandı.");
+                
+            // } 
+
+            try
+            {
+                // int a = int.Parse(null);
+               // int a = int.Parse("test");   
+                int a = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş bir değer girdiniz...");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil...");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz...");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Başarıyla Tamamlandı...");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
