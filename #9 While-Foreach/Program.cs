9315c88dd653a13afb1c8f99a48a2fe926373756
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-02-01 01:31:40 +0300

    While-Foreach lesson at Kodluyoruz

---
+++ #9 While-Foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _9_While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            // 1 den başlayarak console dan girilen sayıya kadar sayı dahil ortalama hesaplayıp konsola yazdıran program.
+            Console.WriteLine("Lütfen bir sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam =0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac  ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+             // 'a' dan  'z' ye kadar tüm harfleri yazdıralım.
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character ++;     
+            }
+            Console.WriteLine("");
+            Console.WriteLine("***** Foreach *****");
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
