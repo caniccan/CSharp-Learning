da058b5953d08a84a1a8ef826257d2988b0cd9e8
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-02-01 15:49:46 +0300

    Arrays lesson at Kodluyoruz

---
+++ #10 Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _10_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dizi Tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            // Dizilere değer atama ve erişim
+            renkler[0]= "Mavi";
+            Console.WriteLine(hayvanlar[1]);
+            dizi[3]=10;
+
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            // Döngüler ile dizi kullanımı
+            // Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz:");
+            int diziuzunlugu = int.Parse(Console.ReadLine());
+            int[] sayıdizisi = new int[diziuzunlugu];
+
+            for (int i = 0; i < diziuzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz: ", i+1);
+                sayıdizisi[i]= int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayıdizisi)
+                toplam += sayi;
+            
+            Console.WriteLine("Ortalama: "+ toplam/diziuzunlugu);
+            Console.ReadKey();
+            
+        }
+    }
+}
